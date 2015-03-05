f7bf4cd6a1ab55387496700eaecc38e600cad23c
Josh Strupp <joshstrupp@gmail.com>
2015-03-04 20:58:50 -0500

    Portfolio Start

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+        $(".fancybox").fancybox();
+    });
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#fff', '#FCFFF5', '#D1DBBD', '#91AA9D', '#3E606F', '#193441', '#11232B', '#04080A'],
+        css3: true
+    });
+});
